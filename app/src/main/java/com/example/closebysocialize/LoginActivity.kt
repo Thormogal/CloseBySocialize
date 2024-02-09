ebcc1682f224af5c5fbcc4ef018705ea6283ee02
DESKTOP-UIEJSCU\oTriks <maar_tiin@hotmail.com>
2024-02-09 11:22:00 +0100

    fix crash

--- app/src/main/java/com/example/closebysocialize/LoginActivity.kt
+++ app/src/main/java/com/example/closebysocialize/LoginActivity.kt
@@ -3,11 +3,9 @@ package com.example.closebysocialize
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
-import android.util.Log
 import android.widget.Button
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
+
 
 class LoginActivity : AppCompatActivity() {
 
