6315e63f90dbdbe37f32161e0343d87641626415
A.saidan <35293299+Ahmadsaidan@users.noreply.github.com>
2024-02-12 16:07:59 +0100

    Done with AddEvent Design

--- app/src/main/java/com/example/closebysocialize/AddEventFragment.kt
+++ app/src/main/java/com/example/closebysocialize/AddEventFragment.kt
@@ -1,28 +1,19 @@
 package com.example.closebysocialize
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.fragment.app.Fragment
-import com.example.closebysocialize.AddEventFragment.AddEventFragment
-import java.util.*
+import com.google.android.material.textfield.TextInputEditText
+import java.util.Calendar
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddEventFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddEventFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -38,44 +29,47 @@ class AddEventFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_event, container, false)
     }
 
-
-    class AddEventFragment : Fragment() {
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
 
-            val editTextEventDate = view.findViewById<EditText>(R.id.edit_event_date)
-            editTextEventDate.setOnClickListener {
-                val calendar = Calendar.getInstance()
-                val year = calendar.get(Calendar.YEAR)
-                val month = calendar.get(Calendar.MONTH)
-                val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val eventDateEditText = view.findViewById<TextInputEditText>(R.id.event_date)
+        eventDateEditText.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+            val minute = calendar.get(Calendar.MINUTE)
 
-                val datePickerDialog = DatePickerDialog(requireContext(), { _, selectedYear, selectedMonth, selectedDay ->
-                    val selectedDate = "${selectedDay}/${selectedMonth + 1}/$selectedYear"
-                    editTextEventDate.setText(selectedDate)
-                }, year, month, day)
-
-                datePickerDialog.show()
-            }
+            val datePickerDialog = DatePickerDialog(
+                it.context,
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                    val timePickerDialog = TimePickerDialog(
+                        it.context,
+                        { _, selectedHour, selectedMinute ->
+                            val selectedTime = "$selectedHour:$selectedMinute"
+                            eventDateEditText.setText("$selectedDate $selectedTime")
+                        },
+                        hour,
+                        minute,
+                        true
+                    )
+                    timePickerDialog.show()
+                },
+                year,
+                month,
+                day
+            )
+            datePickerDialog.show()
         }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddEventFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             AddEventFragment().apply {
