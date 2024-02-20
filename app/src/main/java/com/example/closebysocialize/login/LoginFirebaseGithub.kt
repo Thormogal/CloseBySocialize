e557b53cd2aa05f4b3fda170514b6af50ae04e00
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-02-20 17:12:31 +0100

    Added functionality for Github sign in. Changed the icon for the app when shown on phones. Restructured the code in LoginActivity

---
+++ app/src/main/java/com/example/closebysocialize/login/LoginFirebaseGithub.kt
@@ -0,0 +1,35 @@
+package com.example.closebysocialize.login
+
+import android.app.Activity
+import android.content.Intent
+import android.widget.Toast
+import com.example.closebysocialize.ContainerActivity
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.OAuthProvider
+
+class LoginFirebaseGithub(private val activity: Activity) {
+
+    fun signInWithGitHub() {
+        val provider = OAuthProvider.newBuilder("github.com")
+
+        val auth = FirebaseAuth.getInstance()
+        auth.startActivityForSignInWithProvider(activity, provider.build())
+            .addOnSuccessListener { authResult ->
+                handleSignInResult(authResult)
+            }
+            .addOnFailureListener {
+                Toast.makeText(activity, "Github sign-in failed: ${it.message}", Toast.LENGTH_LONG).show()
+            }
+
+    }
+
+    private fun handleSignInResult(authResult: AuthResult) {
+        val user = authResult.user
+        if (user != null) {
+            val intent = Intent(activity, ContainerActivity::class.java)
+            activity.startActivity(intent)
+            activity.finish()
+        }
+    }
+}
