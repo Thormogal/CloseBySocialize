520518d4dc8e49c5c2f45ae452f6cab7061d7365
Fridisens <fridisens@gmail.com>
2024-02-21 14:30:51 +0100

    profilepicture config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
