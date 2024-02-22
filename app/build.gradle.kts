5af9edf6bb3c380db83d28596a99228d2c3e82ea
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-02-22 19:28:43 +0100

    Fixed so that when you log out you can't press the back button to get back to where you were. Added Facebook login (at the moment, only people invited as testers can test this function).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 }
 
 dependencies {
+    implementation("com.facebook.android:facebook-android-sdk:16.3.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
