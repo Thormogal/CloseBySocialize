05ec8228fbb7a92c753b2915e0d8e6755fc1c5c8
Fridisens <fridisens@gmail.com>
2024-02-28 09:46:29 +0100

    mapstyle updated

--- app/src/main/java/com/example/closebysocialize/map/MapFragment.kt
+++ app/src/main/java/com/example/closebysocialize/map/MapFragment.kt
@@ -1,12 +1,8 @@
 package com.example.closebysocialize.map
 
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.health.connect.datatypes.ExerciseRoute
 import android.os.Build
 import android.location.Location
-
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -14,16 +10,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
 import com.example.closebysocialize.R
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.common.io.Resources
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
@@ -58,6 +52,16 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
+        try {
+            googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireContext(), R.raw.mapstyle
+                )
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
         //longlat for sthlm
         val initialLocation = LatLng(59.3293, 18.0686)
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialLocation, 6f))
